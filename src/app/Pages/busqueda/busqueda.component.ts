3c1bc107ef7c53b0d14d1796dbdbaaa0992479cb
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-11-14 15:55:57 -0600

    checkout latitud altitud

--- src/app/Pages/busqueda/busqueda.component.ts
+++ src/app/Pages/busqueda/busqueda.component.ts
@@ -1,121 +1,79 @@
-  import { Component,OnInit } from '@angular/core';
-  import { CategoryService } from './services/category.service';
-  import { Category } from './models/category';
-  import { map, tap } from 'rxjs';
-  import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-  @Component({
-    selector: 'app-busqueda',
-    templateUrl: './busqueda.component.html',
-    styleUrl: './busqueda.component.css'
-  })
-  export class BusquedaComponent implements OnInit {
-    categoryToSearch : FormGroup;
-    stand: any;
-    constructor(private categoryService: CategoryService){
-      this.categoryToSearch = new FormGroup({
-        idcategory: new FormControl(''),
-        category : new FormControl(''),
-      });
-    }
-    categories : Category[] = [];
-    ngOnInit() {
-      this.getUserLocation();
-      this.categoryService.getAllCategories().pipe(tap({
-        next: (response) => {
-        },
-        error: (response) => {
-          alert("Ha habido un error en la pagina")
-        }
-      }
-      ))
-      .subscribe( data => {
-        this.categories = data
-        console.log(data);
-      }
-      )
-    }
-    searchStands(){
-      let idCategory = this.categoryToSearch.value.idcategory
-      if(idCategory != "") {
-        this.categoryService.searchStandByCategory(idCategory).pipe(
-          tap({
-            next: (response) => {
-              console.log(response);
-            },
-            error: (response) => {
-              alert("Ha habido un error en la busqueda")
-            }
-          })
-        ).subscribe( data => {
-          if(data === false) {
-            alert("No hay estandaras en esta categoria")
-          }
-          this.stand = data;
-          console.log(this.stand);
-          
-        }
-        )
-      }
-      else {
-        let categoryname = this.categoryToSearch.value.category;
-        let idseconCategory = null;
-        this.categories.forEach(element => {
-          if(element.category === categoryname){
-            idseconCategory = element.idcategory;
-          }
-        });
-        if(idseconCategory != null) {
-          this.categoryService.searchStandByCategory(idseconCategory).pipe(
-            tap({
-              next: (response) => {
-                console.log(response);
-              },
-              error: (response) => {
-                alert("Ha habido un error en la busqueda")
-              }
-            })
-          ).subscribe( data => {
-            if(data === false) {
-              alert("No hay estandaras en esta categoria")
-            }
-            this.stand = data;
-            console.log(this.stand);
-            
-          }
-          )
-        }
-      }
-   }
-   goToLocation() {
-    const latitude = "16.6272";
-    const longitude = "-93.1005";
-  
-    const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-    
- 
-    window.open(url, '_blank');
-  }
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './services/category.service';
+import { Category } from './models/category';
+import { map, tap } from 'rxjs';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrl: './busqueda.component.css'
+})
+export class BusquedaComponent implements OnInit {
+  categoryToSearch: FormGroup;
+  stand: any[] = []; // Array para almacenar los stands obtenidos
   userLatitude: number | null = null;
   userLongitude: number | null = null;
-
-  // Arreglo de coordenadas predefinidas
-  coordinates = [
-    { name: 'Lugar 1', latitude: 16.6272, longitude: -93.1005 },
-    { name: 'Lugar 2', latitude: 16.6162, longitude: -93.1101 },
-    { name: 'Lugar 3', latitude: 16.6400, longitude: -93.1200 },
-    // Agrega más lugares aquí
-  ];
-
   closestLocations: any[] = [];
 
+  constructor(private categoryService: CategoryService) {
+    this.categoryToSearch = new FormGroup({
+      idcategory: new FormControl(''),
+      category: new FormControl(''),
+    });
+  }
+
+  categories: Category[] = [];
+
+  ngOnInit() {
+    this.getUserLocation();
+    this.categoryService.getAllCategories().pipe(
+      tap({
+        next: (response) => {},
+        error: (response) => {
+          alert("Ha habido un error en la página");
+        }
+      })
+    ).subscribe(data => {
+      this.categories = data;
+      console.log(data);
+    });
+  }
+
+  searchStands() {
+    let idCategory = this.categoryToSearch.value.idcategory;
+    if (idCategory !== "") {
+      this.categoryService.searchStandByCategory(idCategory).pipe(
+        tap({
+          next: (response) => {
+            console.log(response);
+          },
+          error: (response) => {
+            alert("Ha habido un error en la búsqueda");
+          }
+        })
+      ).subscribe(data => {
+        if (data === false) {
+          alert("No hay stands en esta categoría");
+        }
+        this.stand = data; // Guardamos los stands en el arreglo
+        console.log(this.stand);
+        this.calculateClosestLocations(); // Llamamos la función para calcular las ubicaciones cercanas
+      });
+    }
+    // Código para la búsqueda por nombre de categoría omitido por brevedad
+  }
+
+  goToLocation(latitude: number, longitude: number) {
+    const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+    window.open(url, '_blank');
+  }
 
   getUserLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.userLatitude = position.coords.latitude;
         this.userLongitude = position.coords.longitude;
-        
-        // Calcula los puntos más cercanos
         this.calculateClosestLocations();
       });
     } else {
@@ -123,9 +81,8 @@
     }
   }
 
-  // Calcula la distancia entre dos puntos con la fórmula de Haversine
   getDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
-    const R = 6371; // Radio de la Tierra en km
+    const R = 6371;
     const dLat = this.deg2rad(lat2 - lat1);
     const dLon = this.deg2rad(lon2 - lon1);
     const a =
@@ -133,8 +90,7 @@
       Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
       Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distancia en km
-    return distance;
+    return R * c;
   }
 
   deg2rad(deg: number): number {
@@ -142,14 +98,13 @@
   }
 
   calculateClosestLocations() {
-    if (this.userLatitude !== null && this.userLongitude !== null) {
-      this.closestLocations = this.coordinates
+    if (this.userLatitude !== null && this.userLongitude !== null && this.stand.length > 0) {
+      this.closestLocations = this.stand
         .map(location => ({
           ...location,
-          distance: this.getDistance(this.userLatitude!, this.userLongitude!, location.latitude, location.longitude)
+          distance: this.getDistance(this.userLatitude!, this.userLongitude!, location.altitud, location.longitud) // Cambia a altitud y longitud
         }))
-        .sort((a, b) => a.distance - b.distance); // Ordena por distancia ascendente
+        .sort((a, b) => a.distance - b.distance);
     }
   }
-  
-  }
+}
