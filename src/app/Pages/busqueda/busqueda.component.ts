c4895b6d1c9bc3abe529022e8b016261b5e486cd
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-11-14 16:03:38 -0600

    reject conflicts

--- src/app/Pages/busqueda/busqueda.component.ts
+++ src/app/Pages/busqueda/busqueda.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from './services/category.service';
 import { Category } from './models/category';
@@ -13,105 +12,6 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 export class BusquedaComponent implements OnInit {
   categoryToSearch: FormGroup;
   stand: any[] = []; // Array para almacenar los stands obtenidos
-=======
-  import { Component,OnInit } from '@angular/core';
-  import { CategoryService } from './services/category.service';
-  import { Category } from './models/category';
-  import { map, tap } from 'rxjs';
-  import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-  @Component({
-    selector: 'app-busqueda',
-    templateUrl: './busqueda.component.html',
-    styleUrl: './busqueda.component.css'
-  })
-  export class BusquedaComponent implements OnInit {
-    categoryToSearch : FormGroup;
-    stand: any;
-    constructor(private categoryService: CategoryService){
-      this.categoryToSearch = new FormGroup({
-        idcategory: new FormControl(''),
-        category : new FormControl(''),
-      });
-    }
-    categories : Category[] = [];
-    ngOnInit() {
-      this.getUserLocation();
-      this.categoryService.getAllCategories().pipe(tap({
-        next: (response) => {
-        },
-        error: (response) => {
-          alert("Ha habido un error en la pagina")
-        }
-      }
-      ))
-      .subscribe( data => {
-        this.categories = data
-        console.log(data);
-      }
-      )
-    }
-    searchStands(){
-      let idCategory = this.categoryToSearch.value.idcategory
-      if(idCategory != "") {
-        this.categoryService.searchStandByCategory(idCategory).pipe(
-          tap({
-            next: (response) => {
-              console.log(response);
-            },
-            error: (response) => {
-              alert("Ha habido un error en la busqueda")
-            }
-          })
-        ).subscribe( data => {
-          if(data === false) {
-            alert("No hay estandaras en esta categoria")
-          }
-          this.stand = data;
-          console.log(this.stand);
-          
-        }
-        )
-      }
-      else {
-        let categoryname = this.categoryToSearch.value.category;
-        let idseconCategory = null;
-        this.categories.forEach(element => {
-          if(element.category === categoryname){
-            idseconCategory = element.idcategory;
-          }
-        });
-        if(idseconCategory != null) {
-          this.categoryService.searchStandByCategory(idseconCategory).pipe(
-            tap({
-              next: (response) => {
-                console.log(response);
-              },
-              error: (response) => {
-                alert("Ha habido un error en la busqueda")
-              }
-            })
-          ).subscribe( data => {
-            if(data === false) {
-              alert("No hay estandaras en esta categoria")
-            }
-            this.stand = data;
-            console.log(this.stand);
-            
-          }
-          )
-        }
-      }
-   }
-   goToLocation() {
-    const latitude = "16.615491448112827";
-    const longitude = "-93.08950717304513";
-  
-    const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-    
- 
-    window.open(url, '_blank');
-  }
->>>>>>> main
   userLatitude: number | null = null;
   userLongitude: number | null = null;
   closestLocations: any[] = [];
@@ -202,7 +102,7 @@ export class BusquedaComponent implements OnInit {
       this.closestLocations = this.stand
         .map(location => ({
           ...location,
-          distance: this.getDistance(this.userLatitude!, this.userLongitude!, location.altitud, location.longitud) // Cambia a altitud y longitud
+          distance: this.getDistance(this.userLatitude!, this.userLongitude!, location.altitud, location.longitud)
         }))
         .sort((a, b) => a.distance - b.distance);
     }
