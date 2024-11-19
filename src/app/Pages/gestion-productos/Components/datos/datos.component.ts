ccd91d5b142505d82662ea99d0e7ec9fd8414967
Héctor Somer <233424@ids.upchiapas.edu.mx>
2024-11-19 10:38:58 -0600

    ..

--- src/app/Pages/gestion-productos/Components/datos/datos.component.ts
+++ src/app/Pages/gestion-productos/Components/datos/datos.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { tap } from 'rxjs';
+import { Product } from '../../Models/product';
+import { ProductsService } from '../../service/products.service';
+import { CategoryProduct } from '../../Models/categoryProduct';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-datos',
   templateUrl: './datos.component.html',
   styleUrl: './datos.component.css'
 })
-export class DatosComponent {
+export class DatosComponent{
   product: FormGroup;
-
-  constructor(){
+  categorys: CategoryProduct[] = []
+  constructor(readonly productService: ProductsService){
     this.product = new FormGroup({
       nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      cantidad: new FormControl('', [Validators.required, Validators.minLength(1)]),
       precio: new FormControl('', [Validators.required, Validators.min(1)]),
       descripcion: new FormControl('', [Validators.required]),
       categoria: new FormControl('', [Validators.required])
     });
-  }
-
-  agregarProducto(){
-    
   }
 }
--- src/app/Pages/gestion-productos/service/products.service.ts
+++ src/app/Pages/gestion-productos/service/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../Models/product';
+import { CategoryProduct } from '../Models/categoryProduct';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,5 +13,13 @@ export class ProductsService {
   getProducts(): Observable<Product[]> {
     let url = 'http://52.72.44.45:8000/api/products/';
     return this.httpClient.get<Product[]>(url);
+  }
+  getCategorys(): Observable<CategoryProduct[]> {
+    let url = 'http://52.72.44.45:8000/api/categoryproducts/';
+    return this.httpClient.get<CategoryProduct[]>(url);
+  }
+  postProduct(product: any): Observable<Product>{
+    let url = 'http://52.72.44.45:8000/api/products/';
+    return this.httpClient.post<Product>(url, product);
   }
 }
