bebc93f2093f05e52228b6249336abbead843378
david guzman <reyguz421@gmail.com>
2024-11-21 10:37:26 -0600

    tabla y datos

--- src/app/Pages/gestion-productos/Components/tabla/tabla.component.ts
+++ src/app/Pages/gestion-productos/Components/tabla/tabla.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
-
+import { OnInit, Output, EventEmitter } from '@angular/core';
+import { ProductsService } from '../../service/products.service';
+import { tap } from 'rxjs';
+import { Product } from '../../Models/product';
+import { stringify } from 'querystring';
 @Component({
   selector: 'app-tabla',
   templateUrl: './tabla.component.html',
   styleUrl: './tabla.component.css'
 })
-export class TablaComponent {
+export class TablaComponent implements OnInit {
+  @Output() enviarId = new EventEmitter<string>();
+  @Output() enviarProducts = new EventEmitter<any[]>();
+  products: Product[] = [];
+  indexProduct: number = 0;
+  idProduct: number = 0;
 
+  constructor(private productService:ProductsService) { }
+  ngOnInit(): void {
+    const idStand = Number(localStorage.getItem('standId'));
+
+    this.productService.getProductId(idStand).pipe(tap({
+      next: (response) => {
+        console.log("los productos del usuario es",response);
+        this.products = response;
+      },
+      error: (err) => {
+        console.error('Error al obtener los productos', err);
+      }
+    })).subscribe();
+
+    
+  }
+  @Output() actualizarProducto = new EventEmitter<{index: number, id: number}>
+  actualizar(index: number, id: number): void {
+    this.indexProduct = Number(index);
+    this.idProduct = Number(id);
+  
+  this.actualizarProducto.emit({index: this.indexProduct, id: this.idProduct});
+  alert("Actualizar en el producto: " + this.indexProduct);
+  
+  this.enviarProducts.emit(this.products);
+  }
+
+  eliminar(id: number){
+        this.indexProduct = Number(id);
+
+        this.productService.deletedProduct(this.indexProduct).pipe(tap({
+          next: (response) => {
+            if(response){
+              console.log('Producto eliminado con éxito');
+            }else{
+              console.log('Este producto no existe');
+            }
+            this.ngOnInit();
+          },
+          error: (err) => {
+            console.error('Error al eliminar el producto', err);
+          }
+        })).subscribe()
+  }
+  
 }
